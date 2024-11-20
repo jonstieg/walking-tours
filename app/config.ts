05530ae1bc17022d3fb042374dc88554155f6814
Jon Stieglitz <jonstieg@gmail.com>
2024-11-20 10:46:35 -0500

    new images and favicon

--- app/config.ts
+++ app/config.ts
@@ -1,8 +1,8 @@
 export const metaData = {
-  baseUrl: "https://nextfolio-template.vercel.app/",
+  baseUrl: "https://nycwalkingtours.org/",
   title: "NYC Walking Tours",
   name: "Jon",
-  ogImage: "/opengraph-image.png",
+  // ogImage: "/opengraph-image.png",
   description:
     "",
 };
--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export const metadata: Metadata = {
   },
   description: metaData.description,
   openGraph: {
-    images: metaData.ogImage,
+    // images: metaData.ogImage,
     title: metaData.title,
     description: metaData.description,
     url: metaData.baseUrl,
@@ -41,7 +41,7 @@ export const metadata: Metadata = {
     card: "summary_large_image",
   },
   icons: {
-    icon: "/favicon.ico",
+    icon: "/favicon.png",
   },
 };
 
